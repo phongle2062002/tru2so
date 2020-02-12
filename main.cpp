af3ee66fedf904fbb5a9c9ef874d62aec49ae7c9
phongle2062002 <phongle2062002@gmail.com>
2020-02-12 19:38:01 +0700

    demo1

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	float a,b,x;
+	cout<<"nhap so a";
+	cin>>a;
+	cout<<"nhap so b";
+	cin>>b;
+	x=a-b;
+	cout<<"tru 2 so"<<x;
+	
+	return 0;
+}
+
+
+
+
